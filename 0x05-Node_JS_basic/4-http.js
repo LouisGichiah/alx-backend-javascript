3d3902e6c4959c9e568efae0cc505af4e6f255a3
LouisGichiah <louiegpaul@gmail.com>
2024-05-04 00:44:01 +0300

    trial 1

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,9 +7,9 @@ const app = http.createServer();
 app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
 
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
   res.write(Buffer.from(responseText));
 });
 
