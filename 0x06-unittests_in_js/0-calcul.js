f8f1cf4fbf40cae7df61a0a7b173d52377a250b2
LouisGichiah <louiegpaul@gmail.com>
2024-05-03 22:14:34 +0300

    calculate

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,11 @@
+function calculateNumber(a, b) {
+    // Round the numbers
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+   
+    // Return the sum
+    return roundedA + roundedB;
+   }
+   
+   module.exports = calculateNumber;
+   
